6ff1fc7c448749574cadca202a563d29bbddd6c0
Dimitri <kudryashovda@gmail.com>
2020-04-15 23:26:02 +0300

    Update access to array from structure.cpp

--- access to array from structure.cpp
+++ access to array from structure.cpp
@@ -4,17 +4,19 @@ u_int8_t packet[] = {0xAA,0xFF,0xCA,0xFF};
 
 struct pkt_st
 {   
- u_int16_t head;
- u_int16_t data;
+ u_int16_t header;
+ u_int8_t data;
+ u_int8_t crc;
 };
 
 
 int main()
 {
     pkt_st* p = ( pkt_st* )packet;
-
-    std::cout << "head: " << p->head << std::endl;
-    std::cout << "data: " << p->data << std::endl;
+    
+    std::cout << "head: " << (int)p->header << std::endl;
+    std::cout << "data: " << (int)p->data << std::endl;
+    std::cout << "crc:  " << (int)p->crc  << std::endl;
     
     return 0;
 }
