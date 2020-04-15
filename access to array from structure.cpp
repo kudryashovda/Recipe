dfa8653cc3b2a866a00e138eb68f6bfb508024f7
Dimitri <kudryashovda@gmail.com>
2020-04-15 23:47:10 +0300

    c++98

--- access to array from structure.cpp
+++ access to array from structure.cpp
@@ -1,22 +1,21 @@
 #include <iostream>
 
-u_int8_t packet[] = {0xAA,0xFF,0xCA,0xFF};
+unsigned char packet[] = {0x00,0xFF,0xAA,0xBB};
 
 struct pkt_st
 {   
- u_int16_t header;
- u_int8_t data;
- u_int8_t crc;
+ unsigned short header;
+ unsigned char data;
+ unsigned char crc;
 };
-
 
 int main()
 {
-    pkt_st* p = ( pkt_st* )packet;
+    pkt_st * p = ( pkt_st * )packet;
     
-    std::cout << "head: " << (int)p->header << std::endl;
-    std::cout << "data: " << (int)p->data << std::endl;
-    std::cout << "crc:  " << (int)p->crc  << std::endl;
+    std::cout << "head: " << (int)p->header << std::endl; // 0xFF00
+    std::cout << "data: " << (int)p->data   << std::endl;
+    std::cout << "crc:  " << (int)p->crc    << std::endl;
     
     return 0;
 }
