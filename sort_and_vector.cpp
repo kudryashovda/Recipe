68e25f39f2b3fc6e716bf46eb3d7b56fd4149d0f
Dimitri <kudryashovda@gmail.com>
2020-04-26 14:46:55 +0300

    update sort with lambda

--- sort_and_vector.cpp
+++ sort_and_vector.cpp
@@ -4,17 +4,12 @@
 
 using namespace std;
 
-bool cmp(int a, int b)
-{
-    return (a < b); // 1,2,3,4,5 | if (b <a) then 5,4,3,2,1
-}
-
 int main()
 {
     vector<int> arr = {6,2,1,10,5};
     
-    sort(arr.begin(), arr.end(), cmp);
-
+    sort(arr.begin(), arr.end(), [](const int& a, const int& b) {return (a < b); } );
+    
     for (auto it: arr)
         cout << it << " " ;
     cout << endl;
